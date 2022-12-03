d7d7105eca9dea9d8eeb955cff3d451ea871597a
usernaimandrey <anshlyapnikov@yandex.ru>
2022-12-03 18:03:52 +0300

    add new exercise

---
+++ modules/10-basics/50-testing/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+  // BEGIN
+  std::cout << 420262531;
+  // END
+  return 0;
+}
---
+++ modules/10-basics/60-syntax-error/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+  // BEGIN
+  std::cout << "What Is Dead May Never Die";
+  // END
+  return 0;
+}
