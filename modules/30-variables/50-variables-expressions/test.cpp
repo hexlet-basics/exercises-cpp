ecc2392e65035272893b9b6e97675e25463c5e80
usernaimandrey <anshlyapnikov@yandex.ru>
2023-01-25 16:24:37 +0300

    add variable expression

---
+++ modules/30-variables/50-variables-expressions/test.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+
+#ifndef NDEBUG
+#   define ASSERT(condition, message) \
+    do { \
+        if (!(condition)) { \
+            std::cerr << "Assertion `" #condition "` failed in " << __FILE__ \
+                      << " line " << __LINE__ << ": " << message << std::endl; \
+            std::terminate(); \
+        } \
+    } while (false)
+#else
+#   define ASSERT(condition, message) do { } while (false)
+#endif
+
+
+int main() {
+  FILE *fp;
+  fp = popen("./solution.out", "r");
+  std::string expected = "125\n7500";
+  std::string actual = "";
+  char result[sizeof(expected)];
+
+  while(fgets(result, sizeof(result), fp)) {
+      actual += result;
+    }
+   pclose(fp);
+
+  ASSERT(
+    actual == expected,
+    "\nExpected: \"" + actual + "\" to be: \"" + expected +"\"");
+}
