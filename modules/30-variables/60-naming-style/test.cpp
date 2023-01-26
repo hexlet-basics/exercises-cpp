4efce0f453dae76d01e99e5eec34e13e077fc90e
usernaimandrey <anshlyapnikov@yandex.ru>
2023-01-26 11:24:24 +0300

    namiming-style

---
+++ modules/30-variables/60-naming-style/test.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+
+#ifndef NDEBUG
+#   define ASSERT(condition, message) \
+    do { \
+        if (!(condition)) { \
+            std::cerr << "Assertion `" #condition "` failed in " << __FILE__ \
+                      << " line " << __LINE__ << ": " << message << std::endl; \
+            std::terminate(); \
+        } \
+    } while (false)
+#else
+#   define ASSERT(condition, message) do { } while (false)
+#endif
+
+
+int main() {
+  FILE *fp;
+  fp = popen("./solution.out", "r");
+  std::string expected = "-1100\n";
+  char result[sizeof(expected)];
+  fgets(result, sizeof(result), fp);
+  pclose(fp);
+
+  ASSERT(
+    result == expected,
+    "\nExpected: \"" + std::string(result) + "\" to be: \"" + expected +"\"");
+}
