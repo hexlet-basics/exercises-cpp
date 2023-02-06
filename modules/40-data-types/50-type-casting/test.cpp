cb603329daa69a50109ca5c623459dd1124b2fcb
usernaimandrey <anshlyapnikov@yandex.ru>
2023-02-06 17:29:15 +0300

    add exercise type casting

---
+++ modules/40-data-types/50-type-casting/test.cpp
@@ -0,0 +1,18 @@
+#include "../../../include/output_stream_test.h"
+
+int main() {
+  FILE *fp;
+  fp = popen("./solution.out", "r");
+  std::string expected = "165\n9900";
+  std::string actual = "";
+  char result[sizeof(expected)];
+
+  while(fgets(result, sizeof(result), fp)) {
+      actual += result;
+    }
+   pclose(fp);
+
+  ASSERT(
+    actual == expected,
+    "\nExpected: \"" + actual + "\" to be: \"" + expected +"\"");
+}
