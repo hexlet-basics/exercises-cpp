fb36a98ba8793185a0762d568b31f6c90b16a3c4
usernaimandrey <anshlyapnikov@yandex.ru>
2023-03-26 01:16:25 +0300

    new exercise

---
+++ modules/50-functions/10-define-function/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+// BEGIN
+void PrintMoto() {
+  std::cout << "Spring is coming" << std::endl;
+}
+// END
+
+int main() {
+  // BEGIN
+  PrintMoto();
+  // END
+  return 0;
+}
