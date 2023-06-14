1bb6f4b9b76216c8c0b69fe518785e6914c686e4
usernaimandrey <anshlyapnikov@yandex.ru>
2023-06-15 01:40:48 +0300

    exercise function return

---
+++ modules/50-functions/20-function-return/main.cpp
@@ -0,0 +1,9 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "doctest.h"
+#include <string>
+
+// BEGIN
+std::string SayHurrayThreeTimes() {
+  return "hurray! hurray! hurray!";
+}
+// END
---
+++ modules/50-functions/20-function-return/test.cpp
@@ -0,0 +1,5 @@
+#include "main.cpp"
+
+TEST_CASE("testing the sum function") {
+    CHECK_EQ(SayHurrayThreeTimes(), "hurray! hurray! hurray!");
+}
